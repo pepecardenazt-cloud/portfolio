d8e2b750beea272f0aada47d960ebfe71d2c7f2c
pepecardenazt-cloud <pepecardenazt@gmail.com>
2026-06-07 17:27:25 -0500

    Delete Iniciar-CMS.bat

--- Iniciar-CMS.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Servidor Local - CMS Portafolio Pepe Cárdenas
-echo ==========================================
-echo   Iniciando Servidor Local para el CMS
-echo   Acceso: http://localhost:3000/cms.html
-echo ==========================================
-start "" "http://localhost:3000/cms.html"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0serve.ps1"
-pause
